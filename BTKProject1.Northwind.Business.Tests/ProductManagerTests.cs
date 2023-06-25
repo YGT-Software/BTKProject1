2dca649bea1113d4c9b7b0d525f85ee4bfc0be9c
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-06-25 18:25:49 +0300

    Part 2 / Moq Framework

---
+++ BTKProject1.Northwind.Business.Tests/ProductManagerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using BTKProject1.Northwind.DataAccess.Abstract;
+using BTKProject1.Northwind.Business.Concrete.Managers;
+using FluentValidation;
+
+namespace BTKProject1.Northwind.Business.Tests
+{
+    [TestClass]
+    public class ProductManagerTests
+    {
+        [ExpectedException(typeof(ValidationException))]
+        [TestMethod]
+        public void Product_validation_check()
+        {
+            Mock<IProductDal> mock = new Mock<IProductDal>();
+            ProductManager productManager = new ProductManager(mock.Object);
+
+            productManager.Add(new Product());
+        }
+    }
+}
