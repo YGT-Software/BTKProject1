8d0c48fd40ca16811d2d19feea6bdf6f3ae2b424
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-06-25 12:29:52 +0300

    Part 2 / İş Katmanı

---
+++ BTKProject1.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using BTKProject1.Northwind.Business.Abstract;
+using BTKProject1.Northwind.DataAccess.Abstract;
+using BTKProject1.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTKProject1.Northwind.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+    }
+}
